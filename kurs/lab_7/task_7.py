94db4239c6c551e841f73813aed1b42545d968f1
EroshenkoD <okneshore@ukr.net>
2022-11-21 06:53:27 +0300

    fix task_7

--- kurs/lab_7/task_7.py
+++ kurs/lab_7/task_7.py
@@ -9,55 +9,74 @@ from collections import namedtuple
 
 
 class Truck(ABC):
+
     def __init__(self):
         self.truck_model = "Volvo"
 
-    def we_are(self):
+    def get_truck_model(self):
         return self.truck_model
 
     @abstractmethod
-    def model(self):
+    def get_model(self):
         raise NotImplemented
 
     @abstractmethod
-    def type_truck(self):
+    def get_type_truck(self):
         raise NotImplemented
 
     @abstractmethod
-    def fuel_consumption(self):
+    def get_fuel_consumption(self):
         raise NotImplemented
 
     @abstractmethod
-    def carrying(self):
+    def get_carrying(self):
         raise NotImplemented
 
 
+@dataclass
 class TruckFHTank(Truck):
-    def model(self):
-        print(f"I'm {super().we_are()} FH")
+    pet_name: str
 
-    def type_truck(self):
+    def get_model(self):
+        print(f"I'm {super().get_truck_model()} FH")
+
+    def get_type_truck(self):
         print("For children I drive milk for adults beer")
 
-    def fuel_consumption(self):
+    def get_fuel_consumption(self):
         print("I spend 26 liters if the driver is experienced")
 
-    def carrying(self):
+    def get_carrying(self):
         print("I can pull 30 tons on a good road.")
 
+    def __str__(self):
+        return f"I'm {self.pet_name}"
 
+    def __repr__(self):
+        return f'TruckFHTank({self.pet_name})'
+
+
+@dataclass
 class TruckFLCarTransporter(Truck):
-    def model(self):
-        print(f"I'm {super().we_are()} FL")
+    pet_name: str
 
-    def type_truck(self):
+    def get_model(self):
+        print(f"I'm {super().get_truck_model()} FL")
+
+    def get_type_truck(self):
         print("I drive smaller cars")
 
-    def fuel_consumption(self):
+    def get_fuel_consumption(self):
         print("I spend 22 liters")
 
-    def carrying(self):
+    def get_carrying(self):
         print("I can pull 20 tons")
+
+    def __str__(self):
+        return f"I'm {self.pet_name}"
+
+    def __repr__(self):
+        return f'TruckFLCarTransporter({self.pet_name})'
 
 
 @attrs
@@ -80,31 +99,31 @@ class Cargo:
     size: dict
     list_product: list
 
-    def type_of_truck_for_delivery(self):
+    def get_type_of_truck_for_delivery(self):
         return f"This one will do for {self.id_cargo}"
 
 
-CargoDelivery = namedtuple('CargoDelivery', ['start_point', 'end_point', 'truck', 'cargo'])
+CargoDelivery = namedtuple('CargoDelivery', ['start_point', 'end_point', 'packagetruck', 'packagecargo'])
 
 
 if __name__ == "__main__":
 
-    truck_fo_same_liquid = TruckFHTank()
-    truck_fo_same_car = TruckFLCarTransporter()
-    print(truck_fo_same_liquid.__repr__())
-    print(truck_fo_same_car.__str__())
+    truck_fo_same_liquid = TruckFHTank('truck1')
+    truck_fo_same_car = TruckFLCarTransporter('truck2')
+    print(repr(truck_fo_same_liquid))
+    print(str(truck_fo_same_car))
 
     product_1 = Product('Milk', 'liquid', 10)
     product_2 = Product('Volvo', 'car', 2)
-    print(product_1.__repr__())
-    print(product_2.__str__())
+    print(repr(product_1))
+    print(str(product_2))
 
     cargo_1 = Cargo(1, 10, {'hight': 100, 'width': 150, 'deep': 300}, [product_1])
     cargo_2 = Cargo(2, 20, {'hight': 100, 'width': 150, 'deep': 300}, list(product_2 for _ in range(6)))
-    print(cargo_1.__repr__())
-    print(cargo_2.__str__())
+    print(repr(cargo_1))
+    print(str(cargo_2))
 
     order_1 = CargoDelivery('Odessa', 'Kiev', truck_fo_same_liquid, cargo_1)
     order_2 = CargoDelivery('Odessa', 'Kiev', truck_fo_same_car, cargo_2)
-    print(order_1.__repr__())
-    print(order_2.__str__())
+    print(repr(order_1))
+    print(str(order_2))
